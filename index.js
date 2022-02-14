62fa640c404779be9359a4572f0dbca737352a7d
Walmir Silva <walmirsilva305@gmail.com>
2022-02-13 22:50:45 -0300

    Create a Player

--- index.js
+++ index.js
@@ -1,6 +1,35 @@
+// Init Canvas
 const canvas = document.querySelector('canvas');
+const context = canvas.getContext('2d');
 
-
-
+// Full Screen Canvas
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+
+// Player Class
+class Player {
+    constructor(x, y, radius, color) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.color = color;
+    }
+
+    // Function to Draw Player on Canvas
+    draw() {
+        context.beginPath();
+        context.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+        context.fillStyle = this.color;
+        context.fill();
+    }
+}
+
+
+// Init Player on Center Screen
+const x = canvas.width/2;
+const y = canvas.height/2;
+const player = new Player(x, y, 30, 'blue');
+player.draw();
+
+
+
