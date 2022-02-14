b34d3d10eee83e682cf13d5d88a161d8ca5d8d96
Walmir Silva <walmirsilva305@gmail.com>
2022-02-13 23:30:45 -0300

    Shooting

--- index.js
+++ index.js
@@ -24,12 +24,52 @@ class Player {
     }
 }
 
+// Projectile Class
+class Projectile {
+    constructor(x, y, radius, color, velocity) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.color = color;
+        this.velocity = velocity;
+    }
+
+    draw() {
+        context.beginPath();
+        context.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+        context.fillStyle = this.color;
+        context.fill();
+    }
+
+    update() {
+        this.draw();
+        this.x += this.velocity.x;
+        this.y += this.velocity.y;
+    }
+}
+
+// Animation
+function animate() {
+    requestAnimationFrame(animate);
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    player.draw();
+    projectiles.forEach((projectile) => projectile.update());
+}
 
 // Init Player on Center Screen
 const x = canvas.width/2;
 const y = canvas.height/2;
 const player = new Player(x, y, 30, 'blue');
-player.draw();
 
+// Array of Projectiles
+const projectiles = [];
 
+// Event Click Shoot
+window.addEventListener('click', (event) => {
+    const angle = Math.atan2(event.clientY - y, event.clientX - x);
+    const velocity = {x: Math.cos(angle), y: Math.sin(angle)}
+    projectiles.push(new Projectile(x, y, 5, 'red', velocity));
+})
+
+animate();
 
